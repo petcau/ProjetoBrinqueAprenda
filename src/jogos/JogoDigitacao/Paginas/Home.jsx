38007d55829b258e927f754fcc1cfc05ad0f054b
gabrielBispoTolentino <xodaxuxa.cb@gmail.com>
2025-05-19 15:27:49 -0300

    Componentinação, menu e ramdomização das palavras

--- src/jogos/JogoDigitacao/Paginas/Home.jsx
+++ src/jogos/JogoDigitacao/Paginas/Home.jsx
@@ -1,50 +1,19 @@
+import { useState } from "react";
+import Jogo from "../componenetes/Jogo";
 
-// Para mais tarde ser transferido para os componentes e um menu sera feito
-import React,{useState, useEffect, useRef} from "react";
-export default function Home(){
-
-    const palavra = "dog";
-    const[input, setInput]=useState('');
-    const[mensagem, setmensagem]=useState('');
-    const[tempo, setTempo] = useState('0');
-    const [count, setCount] = useState(0);
-    const timeoutID = useRef(null);
-    const [fim, setFim] = useState(false);
-    // Timer
-    useEffect(() => {
-       timeoutID.current = setTimeout(() => {
-        if (count == 10) {
-          clearTimeout(timeoutID.current);
-          timeoutID.current = null;
-          setFim(true);
-          return;
-        }
-        setCount((count) => count + 1);
-       }, 1000);
-       return () => clearTimeout(timeoutID.current);
-    },[count]); 
-    const mudança =(event) =>{
-        setInput(event.target.value);
-    };
-    // Compara o input do usuario com a palavra correta
-    const comparar = () =>{
-        if (input == palavra ) {
-            alert('Palavra correta')
-        }
-        else{
-            alert('palavra incorreta')
-        }
-    }
-    return(
-    <>
-    <h1>Dog</h1>
-    <h2 style={{color : 'black', display :'flex', ustifyContent: 'center',
-            alignItems: 'center',
-            height: '10vh' }}>{count}</h2>
-    <label>
-        Digite o a palavra escrita acima; <input type="text" value={input} onChange={mudança} placeholder="Digite a palavra"/>
-    </label>
-    <button onClick={comparar}>Comparar</button>
-    </>
-    )
-}
+function Home(){
+   const [estado, setEstado] = useState(true)
+   const irParaJogo = () =>{
+      setEstado(!estado);
+   }
+return(
+   <>
+      <div>
+         {estado ? 
+         <><p>Bem vindo ao jogo da digitação!</p><button onClick={irParaJogo}>Jogar</button></>
+         :  
+         <><button onClick={irParaJogo}>Voltar para a tela inicial</button><Jogo /></>}
+      </div>
+   </>
+)
+}export default Home;
--- src/jogos/JogoDigitacao/componenetes/Jogo.jsx
+++ src/jogos/JogoDigitacao/componenetes/Jogo.jsx
@@ -1,15 +1,40 @@
-import React,{useState, useEffect} from "react";
+
+import React,{useState, useEffect, useRef} from "react";
 export default function Jogo(){
-    const palavra = "dog";
+    const imageUrl = './src/jogos/JogoDigitacao/game_assets/GAMEOVER.png'
+    const palavras = [
+  "cachorro", "gato", "computador", "programar", "teclado", "mouse",
+  "janela", "monitor", "cadeira", "livro", "caneta", "papel", "mochila",
+  "telefone", "internet", "javascript", "react", "vite", "tempo", "digitar",
+  "desafio", "pontuar", "velocidade", "precisão", "jogo", "digitacao"
+];
     const[input, setInput]=useState('');
     const[mensagem, setmensagem]=useState('');
-    const[tempo, setTempo] = useState('');
-    const intervalo = setInterval(() => {
-        
-    }, interval);
+    const[tempo, setTempo] = useState('0');
+    const [count, setCount] = useState(0);
+    const timeoutID = useRef(null);
+    const [fim, setFim] = useState(false);
+    // Timer
+    useEffect(() => {
+       timeoutID.current = setTimeout(() => {
+        if (count == 10) {
+          clearTimeout(timeoutID.current);
+          timeoutID.current = null;
+          setFim(true);
+          return;
+        }
+        setCount((count) => count + 1);
+       }, 1000);
+       return () => clearTimeout(timeoutID.current);
+    },[count]); 
     const mudança =(event) =>{
         setInput(event.target.value);
     };
+    // Aleatorizador de palavras
+     const [palavra, setPalavra] = useState(() => {
+    return palavras[Math.floor(Math.random() * palavras.length)];
+  });
+    // Compara o input do usuario com a palavra correta
     const comparar = () =>{
         if (input == palavra ) {
             alert('Palavra correta')
@@ -20,10 +45,21 @@ export default function Jogo(){
     }
     return(
     <>
-    <label>
-        Digite o a palavra escrita acima; <input type="text" value={input} onChange={mudança} placeholder="Digite a palavra"/>
+    <div>
+    { fim ?
+    <><label>
+    GAME OVER
     </label>
-    <button onClick={comparar}>Comparar</button>
+    <img src={imageUrl}></img></>
+     : <label>
+        <h1>{palavra}</h1>
+    <h2 style={{color : 'black', display :'flex', ustifyContent: 'center',
+            alignItems: 'center',
+         height: '10vh' }}>{count}</h2>
+        Digite o a palavra escrita acima; <input type="text" value={input} onChange={mudança} placeholder="Digite a palavra"/>
+        <button onClick={comparar}>Comparar</button>
+    </label> }
+    </div>
     </>
     )
 }
