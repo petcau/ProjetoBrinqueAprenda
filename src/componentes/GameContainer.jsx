5d7e40b34581d471038885945d56141ccdf3afb4
marktom <bigmarkto5916@gmail.com>
2025-05-21 20:45:17 -0300

    Add capa Anagrama

--- src/componentes/GameContainer.jsx
+++ src/componentes/GameContainer.jsx
@@ -2,13 +2,13 @@ import GameCard from './GameCard'
 import logo from '../assets/card-jornada-bicho.png';
 import capaD from '../assets/CAPADIGITAÇÃO.png';
 import capaA from '../assets/abaco-capa.png';
-
+import capaB from '../assets/anagramacapa4.png';
 
 function GameContainer() {
   return (
       <main className="container">
         <div className="cards">
-          <GameCard route={'/anagrama'} image="" altText="Anagrama" />
+          <GameCard route={'/anagrama'} image={capaB} altText="Anagrama" />
           <GameCard route={'/abacomania'} image={capaA} altText="Abaco Mania" />
           <GameCard route={'/digitacao'} image= {capaD} altText="Digitacao" />
           <GameCard route={'/jornadabicho'} image={logo} altText="Jornada Bicho" />
