4088c262e2018df1ac251abddc6bfb0746ed12d0
Gustavo Henrique <gustavovilanovamatos@gmail.com>
2025-05-29 09:26:48 -0300

    Mudanças jogo da digitação

--- src/jogos/JogoDigitacao/Paginas/Home.jsx
+++ src/jogos/JogoDigitacao/Paginas/Home.jsx
@@ -3,27 +3,16 @@ import Jogo from "../componenetes/Jogo";
 
 function Home(){
    const [estado, setEstado] = useState(true)
-   const [Dificuldade, setDificuldade] = useState();
    const irParaJogo = () =>{
       setEstado(!estado);
-      setDificuldade(2);
    }
-   const irParaJogo1 = () =>{
-      setEstado(!estado);
-      setDificuldade(1);
-   }
-   const irParaJogo3 = () =>{
-      setEstado(!estado);
-      setDificuldade(3);
-   }
+  
 return(
    <>
       <div>
          {estado ? 
          <>
-         <button className="start-button" onClick={irParaJogo1}>Dificuldade facil</button>
-         <button className="start-button" onClick={irParaJogo}>Dificuldade media</button>
-         <button className="start-button" onClick={irParaJogo3}>Dificuldade dificil</button>
+         <button className="start-button" onClick={irParaJogo}>Começar</button>
          </>
          :  
          <><button className="butão" onClick={irParaJogo}>Voltar para a tela inicial</button><Jogo /></>}
