3422159fe50b980d5c6a60779bab6f40103c35d4
EnueLLL1 <Eg121745@gmail.com>
2025-05-29 09:28:17 -0300

    Add Bugs]

--- src/jogos/JornadaBicho/componenetes/LogicaGame/Header.jsx
+++ src/jogos/JornadaBicho/componenetes/LogicaGame/Header.jsx
@@ -1,19 +1,10 @@
-/**
- * Componente que representa o header do jogo JornadaBicho.
- *
- * Renderiza um título com o nível corrente e uma descrição do nível.
- *
- * @param {number} level - O nível atual.
- * @param {string} description - A descrição do nível.
- *
- * @returns {JSX.Element} O componente renderizado.
- */
-// 
-const Header = ({ level, description }) => (
+const Header = ({ level, description, dica }) => (
     <div>
       <h1>Nível {level}</h1>
       <h2>Descrição do Nível</h2>
       <h3>{description}</h3>
+      <h2>Dica</h2>
+      <p>{dica}</p>
     </div>
   );
   
