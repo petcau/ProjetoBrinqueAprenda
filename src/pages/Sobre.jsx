f914b17559904214fa65b3ac6cdb16fb068b924d
marktom <bigmarkto5916@gmail.com>
2025-06-02 21:25:40 -0300

    Add Sobre v0.1

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -5,10 +5,64 @@ function Sobre() {
     <>
       <Header></Header>
       <main className="backSobre">
-        <h1>Sobre o jogo</h1>
-        <p>blah blah blah</p>
-        <h1>Créditos</h1>
-        <p>blah balh balh</p>
+        <div className="sobrecontainer" id="sobrecontainer">
+        <h1 id="sobrecreditos">Sobre o projeto</h1>
+        <p id="textsobre1">
+          A equipe do Brinque Aprenda, por meio desta, manifesta seu sincero agradecimento a todos que contribuíram para sua realização e sucesso.
+          Dentre eles, ao docente Peterson Lobato, ao coordenador Ricardo Peres, ao diretor Isaac Douglas e aos discentes que participaram ativamente do projeto.
+          <br /> <br />Por fim, o projeto agradece à Universidade do Estado da Bahia (UNEB), por meio de seu compromisso com a educação pública, de qualidade e socialmente referenciada.
+          O espaço da UNEB foi essencial para que o Brinque Aprenda se tornasse realidade, reafirmando o papel da universidade como ambiente de formação, inclusão e transformação social.</p>
+          
+        <h1 id="sobrecreditos">Créditos</h1>
+
+        <ul id="textsobre1">
+        GRUPO 00:
+          <li id="listcredito">Cezar Nogueira</li>
+          <li id="listcredito">Gustavo de Sá Mendes</li>
+          <li id="listcredito">Marco Antonio Bomfim Cardoso Dantas</li>
+          <li id="listcredito">Neemias de Melo Vasconcelos</li>
+          <li id="listcredito">Matheus Oliveira Santos</li>
+        </ul>
+        <ul id="textsobre1">
+        GRUPO 01:
+        <li id="listcredito">Everton Oliveira</li> 
+        <li id="listcredito">Pablo de Souza</li>
+        <li id="listcredito">Joedson Nascimento</li>
+        <li id="listcredito">Maria Núbia Avelino</li>
+        <li id="listcredito">Hernandes Pereira</li>
+        </ul>
+        <ul id="textsobre1">
+        GRUPO 02:
+        <li id="listcredito">Daniel Joaquim Batista dos Santos</li>
+        <li id="listcredito">Gustavo Henrique Vila Nova Matos</li>
+        <li id="listcredito">Eduardo de Jesus Barbosa Santos</li>
+        <li id="listcredito">Gabriel Bispo Tolentino</li>
+        </ul>
+        <ul id="textsobre1">
+        GRUPO 03:
+        <li id="listcredito"></li>
+        <li id="listcredito"></li>
+        <li id="listcredito"></li>
+        <li id="listcredito"></li>
+        <li id="listcredito"></li>
+        </ul>
+        <ul id="textsobre1">
+        GRUPO 04:
+        <li id="listcredito">Silas Pereira de Souza Dantas</li>
+        <li id="listcredito">Emanuel Gomes Bispo</li>
+        <li id="listcredito">Joabe Borges da Silva</li>
+        <li id="listcredito">Felipe Dantas Ferreira</li>
+        <li id="listcredito">Jadson Rodrigo Santos Pereira</li>
+        </ul>
+        <h1 id="sobrecreditos">Recursos de Terceiros</h1>
+        <ul id="textsobre1">
+        <li id="listcredito"></li>
+        <li id="listcredito"></li>
+        <li id="listcredito"></li>
+        <li id="listcredito"></li>
+        <li id="listcredito"></li>
+        </ul>
+        </div>
       </main>
     </>
   );
