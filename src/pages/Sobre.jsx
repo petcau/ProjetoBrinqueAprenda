2a5d501277edecb2ed47ce33e8c9670f8fc952b1
marktom <bigmarkto5916@gmail.com>
2025-06-03 20:35:30 -0300

    Almost Finished Sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -17,51 +17,50 @@ function Sobre() {
 
         <ul id="textsobre1">
         GRUPO 00:
-          <li id="listcredito">Cezar Nogueira</li>
-          <li id="listcredito">Gustavo de Sá Mendes</li>
-          <li id="listcredito">Marco Antonio Bomfim Cardoso Dantas</li>
-          <li id="listcredito">Neemias de Melo Vasconcelos</li>
-          <li id="listcredito">Matheus Oliveira Santos</li>
+          <li id="listcredito">Matheus Oliveira Santos - Gestor chefe</li>
+          <li id="listcredito">Cezar Nogueira - Programador chefe</li>
+          <li id="listcredito">Gustavo de Sá Mendes - Programador chefe</li>
+          <li id="listcredito">Neemias de Melo Vasconcelos - Programador chefe</li>
+          <li id="listcredito">Marco Antonio Bomfim Cardoso Dantas - Designer chefe</li>
+          
         </ul>
         <ul id="textsobre1">
         GRUPO 01:
-        <li id="listcredito">Everton Oliveira</li> 
-        <li id="listcredito">Pablo de Souza</li>
-        <li id="listcredito">Joedson Nascimento</li>
-        <li id="listcredito">Maria Núbia Avelino</li>
-        <li id="listcredito">Hernandes Pereira</li>
+        <li id="listcredito">Hernandes Pereira - Gestor</li>
+        <li id="listcredito">Everton Oliveira - Programador</li> 
+        <li id="listcredito">Pablo de Souza - Programador</li>
+        <li id="listcredito">Joedson Nascimento - Designer</li>
+        <li id="listcredito">Maria Núbia Avelino - Designer</li>
+        
         </ul>
         <ul id="textsobre1">
         GRUPO 02:
-        <li id="listcredito">Daniel Joaquim Batista dos Santos</li>
-        <li id="listcredito">Gustavo Henrique Vila Nova Matos</li>
-        <li id="listcredito">Eduardo de Jesus Barbosa Santos</li>
-        <li id="listcredito">Gabriel Bispo Tolentino</li>
+        <li id="listcredito">Daniel Joaquim Batista dos Santos - Gestor</li>
+        <li id="listcredito">Eduardo de Jesus Barbosa Santos - Programador</li>
+        <li id="listcredito">Gabriel Bispo Tolentino - Programador</li>
+        <li id="listcredito">Augusto Rabelo dos Santos - Programador</li>
+        <li id="listcredito">Gustavo Henrique Vila Nova Matos - Designer</li>
         </ul>
         <ul id="textsobre1">
         GRUPO 03:
-        <li id="listcredito"></li>
-        <li id="listcredito"></li>
-        <li id="listcredito"></li>
-        <li id="listcredito"></li>
-        <li id="listcredito"></li>
+        <li id="listcredito">Gustavo Ramon Ribeiro Oliveira Santos - Gestor</li>
+        <li id="listcredito">Vítor Thadeu Martins da Silva Gonzaga - Programador</li>
+        <li id="listcredito">Emanuel Vítor Ramos dos Santos Gonzaga - Programador</li>
+        <li id="listcredito">Thiago Sampaio Santiago Pereira - Designer</li>
+        <li id="listcredito">Rafael da Conceição Cunha - Designer</li>
         </ul>
         <ul id="textsobre1">
         GRUPO 04:
-        <li id="listcredito">Silas Pereira de Souza Dantas</li>
-        <li id="listcredito">Emanuel Gomes Bispo</li>
-        <li id="listcredito">Joabe Borges da Silva</li>
-        <li id="listcredito">Felipe Dantas Ferreira</li>
-        <li id="listcredito">Jadson Rodrigo Santos Pereira</li>
+        <li id="listcredito">Jadson Rodrigo Santos Pereira - Gestor</li>
+        <li id="listcredito">Joabe Borges da Silva - Gestor</li>
+        <li id="listcredito">Emanuel Gomes Bispo - Programador</li>
+        <li id="listcredito">Felipe Dantas Ferreira - Designer</li>
+        <li id="listcredito">Silas Pereira de Souza Dantas - Designer</li>
         </ul>
-        <h1 id="sobrecreditos">Recursos de Terceiros</h1>
-        <ul id="textsobre1">
-        <li id="listcredito"></li>
-        <li id="listcredito"></li>
-        <li id="listcredito"></li>
-        <li id="listcredito"></li>
-        <li id="listcredito"></li>
-        </ul>
+        <h1 id="sobrecreditos">Obrigado por jogar!</h1>
+        <br />
+        <br />
+        <br />
         </div>
       </main>
     </>
