c9767ef55baeb9b001e238baaa2be197dcad093c
Everton Oliveira <justtonoliver@gmail.com>
2025-06-05 07:57:47 -0300

    Remoção de comentarios

--- src/jogos/Anagrama/componenetes/PalavrasDescobertas.jsx
+++ src/jogos/Anagrama/componenetes/PalavrasDescobertas.jsx
@@ -1,25 +1,3 @@
-/*function PalavrasDescobertas({ descobertas, palavrasValidas }) {
-  if (!palavrasValidas || !Array.isArray(palavrasValidas)) {
-    return <div className="erro">Erro: palavras não carregadas.</div>;
-  }
-
-  return (
-    <div className="container-palavras">
-      {palavrasValidas.map((palavra, index) => (
-        <span key={index} className="palavra">
-          <span className="separador">| </span>
-          {descobertas.includes(palavra)
-            ? palavra
-            : "_ ".repeat(palavra.length)}
-          <span className="separador">| </span>
-        </span>
-      ))}
-    </div>
-  );
-}
-
-export default PalavrasDescobertas;*/
-
 function PalavrasDescobertas({ descobertas, palavrasValidas }) {
   if (!palavrasValidas || !Array.isArray(palavrasValidas)) {
     return <div className="erro">Erro: palavras não carregadas.</div>;
--- src/jogos/Anagrama/componenetes/useAnagrama.jsx
+++ src/jogos/Anagrama/componenetes/useAnagrama.jsx
@@ -1,87 +1,3 @@
-/*import { useState, useEffect, useCallback } from "react";
-import PalavrasData from "./Palavras.json";
-
-export function useAnagrama() {
-  const [nivelAtual, setNivelAtual] = useState(0);
-  const [tentativa, setTentativa] = useState("");
-  const [descobertas, setDescobertas] = useState([]);
-  const [tempoEsgotado, setTempoEsgotado] = useState(false); 
-
-  const rodada = PalavrasData.anagramas[nivelAtual];
-  const palavraBase = rodada.palavraEmbaralhada;
-  const palavrasValidas = rodada.palavrasValidas;
-  const letras = palavraBase.split("");
-
-  const adicionarLetra = (letra) => {
-    if (!tempoEsgotado) {
-      setTentativa((t) => t + letra);
-    }
-  };
-
-  const resetarTentativa = () => {
-    setTentativa("");
-  };
-
-  const enviarPalavra = () => {
-    if (tempoEsgotado) return;
-    const palavra = tentativa.toUpperCase();
-    if (palavrasValidas.includes(palavra) && !descobertas.includes(palavra)) {
-      const novas = [...descobertas, palavra];
-      setDescobertas(novas);
-    }
-    resetarTentativa();
-  };
-
-  const limparDescobertas = () => {
-    setDescobertas([]);
-  };
-
-  const lidarComTempoEsgotado = () => {
-    setTempoEsgotado(true); 
-  };
-
-  const reiniciarTempo = () => {
-    setTempoEsgotado(false); 
-    resetarTentativa();
-    limparDescobertas();
-  };
-
-  const proximoNivel = useCallback(() => {
-    if (nivelAtual < PalavrasData.anagramas.length - 1) {
-      setNivelAtual((n) => n + 1);
-      setTentativa("");
-      setDescobertas([]);
-      setTempoEsgotado(true); 
-    }
-  }, [nivelAtual]);
-
-  
-  useEffect(() => {
-    if (
-      palavrasValidas.length > 0 &&
-      descobertas.length === palavrasValidas.length
-    ) {
-      const timer = setTimeout(proximoNivel, 1500);
-      return () => clearTimeout(timer);
-    }
-  }, [descobertas, palavrasValidas, proximoNivel]);
-
-  return {
-    letras,
-    tentativa,
-    descobertas,
-    palavrasValidas,
-    nivelAtual,
-    adicionarLetra,
-    resetarTentativa,
-    enviarPalavra,
-    limparDescobertas,
-    tempoEsgotado,         
-    lidarComTempoEsgotado, 
-    reiniciarTempo         
-  };
-}*/
-
 import { useState, useCallback } from "react";
 import PalavrasData from "./Palavras.json";
 
@@ -149,16 +65,6 @@ export function useAnagrama() {
     }
   }, [nivelAtual]);
 
-  /*useEffect(() => {
-    if (
-      palavrasValidas.length > 0 &&
-      descobertas.length === palavrasValidas.length
-    ) {
-      const timer = setTimeout(proximoNivel, 1500);
-      return () => clearTimeout(timer);
-    }
-  }, [descobertas, palavrasValidas, proximoNivel]);
-*/
   return {
     letras,
     tentativa,
