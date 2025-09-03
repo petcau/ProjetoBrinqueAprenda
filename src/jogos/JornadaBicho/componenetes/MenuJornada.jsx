c229204f2253fe030e50751c3c76624479943fd1
Peterson Albuquerque Lobato <peterson.lobato@gmail.com>
2025-09-03 14:58:00 -0300

    fix5

--- src/jogos/JornadaBicho/componenetes/MenuJornada.jsx
+++ src/jogos/JornadaBicho/componenetes/MenuJornada.jsx
@@ -56,7 +56,7 @@ function MenuJornada({ estado, setEstado }) {
           <figure className="Titulo_JornadaFig">
             <img
               className="Titulo_Jonada_img"
-              src="src/jogos/JornadaBicho/game_assets/imagens/titulo-jornada.png"
+              src="../../game_assets/imagens/titulo-jornada.png"
               alt="Título do jogo Jornada Bicho"
               id="Titulo_Jornada"
             />
